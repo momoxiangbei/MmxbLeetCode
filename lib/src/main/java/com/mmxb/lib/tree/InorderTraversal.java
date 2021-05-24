97de73b17efe00e500535c49736a4c153462352b
momoxiangbei <momoxiangbei@aliyun.com>
2021-05-24 10:31:09 +0800

    二叉树的中序遍历

---
+++ lib/src/main/java/com/mmxb/lib/tree/InorderTraversal.java
@@ -0,0 +1,48 @@
+package com.mmxb.lib.tree;
+
+/**
+ * Created by mmxb on 5/24/21.
+ * <p>
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
